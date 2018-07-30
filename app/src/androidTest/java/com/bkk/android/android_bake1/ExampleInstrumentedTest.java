3bb1336b9a2ca9316bc896b6caae278f6fd9fdac
Andy Murph <murph@email.com>
2018-07-30 09:33:24 -0400

    4, todo: make a very basic Espresso test

--- app/src/androidTest/java/com/bkk/android/android_bake1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/bkk/android/android_bake1/ExampleInstrumentedTest.java
@@ -1,13 +1,32 @@
 package com.bkk.android.android_bake1;
 
+
+import static android.app.Instrumentation.ActivityResult;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+import static junit.framework.Assert.assertEquals;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.hamcrest.core.IsNot.not;
+
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
